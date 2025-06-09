803eb6096ce8bff0cd7a67e1d7ab4d9b8c248cb1
kim <smkim000721@naver.com>
2025-06-09 16:13:11 +0900

    [김성민] 강의별 퀴즈리스트와 (강의,주차)별 퀴즈문제 출력

--- src/main/java/com/example/academylms/mapper/QuizMapper.java
+++ src/main/java/com/example/academylms/mapper/QuizMapper.java
@@ -1,5 +1,22 @@
 package com.example.academylms.mapper;
 
-public interface QuizMapper {
+import java.util.HashMap;
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.example.academylms.dto.Page;
+import com.example.academylms.dto.QuizOption;
+
+@Mapper
+public interface QuizMapper {
+	List<HashMap<String,Object>> quizListByLectureId(int lectureId);
+	List<HashMap<String,Object>> quizOne(@Param("page") Page page, @Param("weekId") int weekId);;
+	int quizTotalCount(int weekId);
+	List<QuizOption> quizOptionList(int quizId);
+	Integer selectJoinId(int weekId, int enrollmentId);
+	int selectLectureIdByweekId(int weekId);
+	int selectEnrollmentId(int studentId, int lectureId);
+	void insertJoinId(int weekId, int enrollmentId);
 }
--- src/main/java/com/example/academylms/service/QuizService.java
+++ src/main/java/com/example/academylms/service/QuizService.java
@@ -1,5 +1,59 @@
 package com.example.academylms.service;
 
-public class QuizService {
+import java.util.HashMap;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.academylms.dto.Page;
+import com.example.academylms.dto.QuizOption;
+import com.example.academylms.mapper.QuizMapper;
+
+@Service
+@Transactional
+public class QuizService {
+	@Autowired QuizMapper quizMapper;
+	
+	// 강의별 퀴즈 리스트
+	public List<HashMap<String,Object>> quizListByLectureId(int lectureId){
+		
+		return quizMapper.quizListByLectureId(lectureId);
+	}
+	
+	// 퀴즈 문제풀이 페이지
+	public List<HashMap<String,Object>> quizOne(Page page, int weekId){
+		return quizMapper.quizOne(page, weekId);
+	}
+	
+	// (강의,주차)별 퀴즈 전체 문제수
+	public int quizTotalCount(int weekId) {
+		return quizMapper.quizTotalCount(weekId);
+	}
+	
+	// 퀴즈(객관식) 보기
+	public List<QuizOption> quizOptionList(int quizId) {
+		return quizMapper.quizOptionList(quizId);
+	}
+	
+	// 퀴즈 응시기록 확인
+	public Integer selectJoinId(int weekId, int enrollmentId) {
+		return quizMapper.selectJoinId(weekId, enrollmentId);
+	}
+	
+	// 퀴즈 응시기록 확인
+	public int selectEnrollmentId(int studentId, int lectureId) {
+		return quizMapper.selectEnrollmentId(studentId, lectureId);
+	}
+	
+	// weekId에 해당하는 lectureId 반환
+	public int selectLectureIdByweekId(int weekId) {
+		return quizMapper.selectLectureIdByweekId(weekId);
+	}
+	
+	// join_id등록
+	public void insertJoinId(int weekId, int enrollmentId) {
+		quizMapper.insertJoinId(weekId, enrollmentId);
+	}
 }
