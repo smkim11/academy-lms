de4b7adfafc93461112e9e0a61043486f36dfbd5
kim <smkim000721@naver.com>
2025-06-11 12:43:37 +0900

    [김성민]문제유형 체크하는 컨트롤러 추가

--- src/main/java/com/example/academylms/rest/QuizRest.java
+++ src/main/java/com/example/academylms/rest/QuizRest.java
@@ -1,5 +1,27 @@
 package com.example.academylms.rest;
 
-public class QuizRest {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.academylms.service.QuizService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class QuizRest {
+	@Autowired QuizService quizService;
+	
+	// 객관식 주관식 체크
+	@GetMapping("/questionType/{type}")
+	public String questionType(@PathVariable String type) {
+		if(type.equals("객관식")) {
+			return "gek";
+		}
+		return "ju";
+	}
+	
 }
